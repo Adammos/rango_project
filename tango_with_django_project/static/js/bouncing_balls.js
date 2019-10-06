de35507a4e3e4425fb47566e202293fc989ef941
Adam <janca.adam89@gmail.com>
2019-10-06 22:47:30 +0200

    bouncing balls count

--- tango_with_django_project/static/js/bouncing_balls.js
+++ tango_with_django_project/static/js/bouncing_balls.js
@@ -5,6 +5,9 @@ var ctx = canvas.getContext('2d');
 
 var width = canvas.width = window.innerWidth;
 var height = canvas.height = window.innerHeight;
+var ballCountPara = document.querySelector('.ball-count');
+var ballCount = 25;
+ballCountPara.textContent = 'Ball Count: ' + ballCount;
 
 // function to generate random number
 
@@ -13,14 +16,102 @@ function random(min,max) {
   return num;
 }
 
-function Ball(x, y, velX, velY, color, size) {
+function Shape(x, y, velX, velY, exists) {
 	this.x = x;
 	this.y = y;
 	this.velX = velX;
 	this.velY = velY;
+	this.exists = exists;
+}
+
+function EvilCircle(x, y, exists) {
+	Shape.call(this, x, y, 20, 20, exists);
+
+	this.color = 'white';
+	this.size = 10;
+}
+EvilCircle.prototype = Object.create(Shape.prototype);
+
+Object.defineProperty(EvilCircle.prototype, 'constructor', {
+value: EvilCircle,
+enumerable: false,
+writable: true
+});
+
+EvilCircle.prototype.draw = function() {
+	ctx.beginPath();
+	ctx.lineWidth = 3;
+	ctx.strokeStyle = this.color;
+	ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+	ctx.stroke();
+}
+
+EvilCircle.prototype.checkBounds = function() {
+if ((this.x + this.size) >= width) {
+		this.x -= (this.size);
+	}
+
+	if ((this.x - this.size) <= 0) {
+		this.x += (this.size);
+	}
+
+	if ((this.y + this.size) >= height) {
+		this.y -= (this.size);
+	}
+
+	if ((this.y - this.size) <= 0) {
+		this.y += (this.size);
+	}	
+}
+
+EvilCircle.prototype.setControls = function() {
+	// because of function scope
+	var _this = this;
+	window.onkeydown = function(e) {
+		if (e.keyCode === 37) {
+			_this.x -= _this.velX;
+		} else if (e.keyCode === 39) {
+			_this.x += _this.velX;
+		} else if (e.keyCode === 38) {
+			_this.y -= _this.velY;
+		} else if (e.keyCode === 40) {
+			_this.y += _this.velY;
+		}
+		console.log(String(e.keyCode) + " was pressed.");
+	}	
+	
+}
+
+EvilCircle.prototype.collisionDetect = function() {
+	for (var j = 0; j < balls.length; j++) {
+		if (balls[j].exists) {
+			var dx = this.x - balls[j].x;
+			var dy = this.y - balls[j].y;
+			var distance = Math.sqrt(dx * dx + dy * dy);
+
+			if (distance < this.size + balls[j].size) {
+				balls[j].exists = false;
+				ballCount-= 1;
+				ballCountPara.textContent = 'Ball Count: ' + ballCount;
+			}
+		}
+	}
+}
+
+function Ball(x, y, velX, velY, exists, color, size) {
+	Shape.call(this, x, y, velX, velY, exists);
+
 	this.color = color;
 	this.size = size;
 }
+Ball.prototype = Object.create(Shape.prototype);
+
+Object.defineProperty(Ball.prototype, 'constructor', {
+value: Ball,
+enumerable: false,
+writable: true
+});
+
 Ball.prototype.draw = function() {
 	ctx.beginPath();
 	ctx.fillStyle = this.color;
@@ -63,7 +154,7 @@ Ball.prototype.collisionDetect = function() {
 }
 var balls = [];
 
-while (balls.length < 25) {
+while (balls.length < ballCount) {
 	var size = random(10, 20);
 	var ball = new Ball(
 		//ball position always drawn at least one ball width
@@ -72,6 +163,7 @@ while (balls.length < 25) {
 		random(0 + size, height - size),
 		random(-7, 7),
 		random(-7, 7),
+		true,
 		'rgb(' + random(0,255) + ', ' + random(0, 255) + ',' + random(0, 255) + ')', 
 		size
 	);
@@ -79,14 +171,23 @@ while (balls.length < 25) {
 	balls.push(ball);
 }
 
+var evilCircle = new EvilCircle(50, 50, true);
+evilCircle.setControls();
+
+
 function loop() {
 	ctx.fillStyle = 'rgb(0, 0, 0, 0.25)';
 	ctx.fillRect(0, 0, width, height);
 
 	for (var i = 0; i < balls.length; i++) {
-		balls[i].draw();
-		balls[i].update();
-		balls[i].collisionDetect();
+		if (balls[i].exists) {
+			balls[i].draw();
+			balls[i].update();
+			balls[i].collisionDetect();
+			evilCircle.draw()
+			evilCircle.checkBounds();
+			evilCircle.collisionDetect();
+		}
 	}
 
 	requestAnimationFrame(loop)
